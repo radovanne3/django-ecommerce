bf2fd5109255f1bafead6881e826c8d6fb6c4681
Snorlax <radovanne3@gmail.com>
2021-10-06 18:27:55 +0200

    first commit

---
+++ shop/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
